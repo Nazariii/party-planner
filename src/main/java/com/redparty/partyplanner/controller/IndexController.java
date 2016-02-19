ed2b28b96567256c2e2438e229740ebcaaac182e
Nazariii <nazarii.ivashkiv@gmail.com>
2016-02-19 12:01:27 +0200

    Add simple oauth2 auth with facebook

--- src/main/java/com/redparty/partyplanner/controller/IndexController.java
+++ src/main/java/com/redparty/partyplanner/controller/IndexController.java
@@ -4,15 +4,22 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:9001")
 public class IndexController extends BaseController {
 
 
-    @RequestMapping("/")
+    @RequestMapping("/welcome")
     @CrossOrigin(origins = "http://localhost:9000")
     public String index() {
-        return "Hy to everybody from party planner!!!";
+        return "Hi to everybody from party planner!!!";
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
     }
 
 }
