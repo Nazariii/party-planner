ce7a45ee65a5fccf0875c5df3bdb37057516de26
Nazariii <nazarii.ivashkiv@gmail.com>
2016-02-29 14:39:25 +0200

    Add validation sample

--- src/main/java/com/redparty/partyplanner/common/domain/User.java
+++ src/main/java/com/redparty/partyplanner/common/domain/User.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.util.*;
 
 @Entity
@@ -15,6 +16,7 @@ import java.util.*;
 public class User extends BaseEntity{
 
     @Column(unique = true, nullable = false)
+    @NotNull
     private String email;
 
     @JsonIgnore
