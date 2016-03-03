4f6440295fc5dc694e3d86428c20838083ca35e7
Nazariii <nazarii.ivashkiv@gmail.com>
2016-03-03 15:17:15 +0200

    Add jdbc user retrieving and BCrypt encription

--- src/main/java/com/redparty/partyplanner/config/SecurityConfig.java
+++ src/main/java/com/redparty/partyplanner/config/SecurityConfig.java
@@ -1,22 +1,33 @@
 package com.redparty.partyplanner.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.encoding.BaseDigestPasswordEncoder;
+import org.springframework.security.authentication.encoding.LdapShaPasswordEncoder;
+import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .inMemoryAuthentication()
-                .withUser("naz").password("1234").roles("USER")
-                .and()
-                .withUser("admin").password("1234").roles("USER", "ADMIN"); // same as .authorities("ROLE_USER", "ROLE_ADMIN")
+                .jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT email as username, password, true FROM user WHERE email=?")
+                .authoritiesByUsernameQuery("SELECT email as username, 'ROLE_USER' FROM user WHERE email=?")
+                .passwordEncoder(new BCryptPasswordEncoder());
     }
 
     @Override
