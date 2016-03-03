b44c3e8e49943e3e899d8896400630a234512c25
Nazariii <nazarii.ivashkiv@gmail.com>
2016-03-03 12:52:59 +0200

    Add simplest spring security with inmemory db

---
+++ src/main/java/com/redparty/partyplanner/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.redparty.partyplanner.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("naz").password("1234").roles("USER")
+                .and()
+                .withUser("admin").password("1234").roles("USER", "ADMIN"); // same as .authorities("ROLE_USER", "ROLE_ADMIN")
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+}
