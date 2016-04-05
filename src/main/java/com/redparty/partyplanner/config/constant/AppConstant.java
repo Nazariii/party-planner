80ddda533d0748dd188fea1250917921d75731f1
Nazariii <nazarii.ivashkiv@gmail.com>
2016-04-05 17:44:52 +0300

    Add ehcache support

---
+++ src/main/java/com/redparty/partyplanner/config/constant/AppConstant.java
@@ -0,0 +1,8 @@
+package com.redparty.partyplanner.config.constant;
+
+public interface AppConstant {
+    String CACHE_NAME = "partyPlanner";
+    String EVENT_CACHE_NAME = "eventCache";
+    String SERVICE_CACHE_NAME = "serviceCache";
+    String USER_CACHE_NAME = "userCache";
+}
