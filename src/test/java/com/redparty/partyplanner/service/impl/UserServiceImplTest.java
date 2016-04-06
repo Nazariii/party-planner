8b21d09dda9009401a04c4157026c79c54d0bd58
Nazariii <nazarii.ivashkiv@gmail.com>
2016-04-06 13:49:06 +0300

    Domain equals and hash refactor

--- src/test/java/com/redparty/partyplanner/service/impl/UserServiceImplTest.java
+++ src/test/java/com/redparty/partyplanner/service/impl/UserServiceImplTest.java
@@ -2,31 +2,8 @@ package com.redparty.partyplanner.service.impl;
 
 import com.redparty.partyplanner.IntegrationTestBase;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class UserServiceImplTest extends IntegrationTestBase {
 
-    @Test
-    public void findUserById() throws Exception {
-
-    }
-
-    @Test
-    public void add() throws Exception {
-
-    }
-
-    @Test
-    public void add1() throws Exception {
-
-    }
-
-    @Test
-    public void findAll() throws Exception {
-
-    }
-
-    @Test
-    public void delete() throws Exception {
-
-    }
 }
