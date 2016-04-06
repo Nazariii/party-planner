20f29d315b3f8773c3cc68f8c070906c45ff8a8f
Nazariii <nazarii.ivashkiv@gmail.com>
2016-04-06 13:48:48 +0300

    Refactor caching and add testing for it

--- src/main/java/com/redparty/partyplanner/service/EventService.java
+++ src/main/java/com/redparty/partyplanner/service/EventService.java
@@ -1,18 +1,26 @@
 package com.redparty.partyplanner.service;
 
 import com.redparty.partyplanner.common.domain.Event;
+import com.redparty.partyplanner.config.constant.AppConstant;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 
 import java.util.List;
 
 public interface EventService {
 
+    @Cacheable(AppConstant.EVENT_CACHE_NAME)
     Event findEventById(Long id);
 
+    @CachePut(value = AppConstant.EVENT_CACHE_NAME, key = "#result.id")
     Event add(Event event);
 
+    @CachePut(value = AppConstant.EVENT_CACHE_NAME, key = "#result.id")
     Event add(String name, Event.EventStatus eventStatus, Long userId);
 
     List<Event> findAll();
 
+    @CacheEvict(AppConstant.EVENT_CACHE_NAME)
     void delete(Long id);
 }
--- src/main/java/com/redparty/partyplanner/service/impl/EventServiceImpl.java
+++ src/main/java/com/redparty/partyplanner/service/impl/EventServiceImpl.java
@@ -4,19 +4,14 @@ import com.redparty.partyplanner.common.domain.Event;
 import com.redparty.partyplanner.common.domain.User;
 import com.redparty.partyplanner.common.exception.ResourceCRUDException;
 import com.redparty.partyplanner.common.exception.ResourceNotFoundException;
-import com.redparty.partyplanner.config.constant.AppConstant;
 import com.redparty.partyplanner.repository.EventRepository;
 import com.redparty.partyplanner.repository.UserRepository;
 import com.redparty.partyplanner.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @Transactional
@@ -29,21 +24,18 @@ public class EventServiceImpl implements EventService {
     private UserRepository userRepository;
 
     @Override
-    @Cacheable(AppConstant.EVENT_CACHE_NAME)
     public Event findEventById(Long id) throws ResourceNotFoundException {
         return eventRepository.findOne(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Event", "id", id));
     }
 
     @Override
-    @CachePut(value = AppConstant.EVENT_CACHE_NAME, key = "#result.id")
     public Event add(Event event) {
         return eventRepository.save(event)
                 .orElseThrow(() -> new ResourceCRUDException("Event", "Name", event.getName(), "Created"));
     }
 
     @Override
-    @CachePut(value = AppConstant.EVENT_CACHE_NAME, key = "#result.id")
     public Event add(String name, Event.EventStatus eventStatus, Long userId) {
         User user = userRepository.findOne(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("User", "id", userId));
@@ -56,7 +48,6 @@ public class EventServiceImpl implements EventService {
     }
 
     @Override
-    @CacheEvict(AppConstant.EVENT_CACHE_NAME)
     public void delete(Long id) {
         eventRepository.delete(id);
     }
