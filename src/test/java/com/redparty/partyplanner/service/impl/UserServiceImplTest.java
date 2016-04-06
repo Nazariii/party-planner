4176a0dce1c6cfd4d10364974b453fc4df569e94
Nazariii <nazarii.ivashkiv@gmail.com>
2016-04-06 15:09:42 +0300

    Fix CI test failure

--- src/test/java/com/redparty/partyplanner/service/impl/UserServiceImplTest.java
+++
@@ -1,9 +0,0 @@
-package com.redparty.partyplanner.service.impl;
-
-import com.redparty.partyplanner.IntegrationTestBase;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-
-public class UserServiceImplTest extends IntegrationTestBase {
-
-}
