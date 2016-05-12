c757b6bf521c05eec03730321535553f7c58c757
Nazariii <nazarii.ivashkiv@gmail.com>
2016-05-12 17:06:08 +0300

    Add login controller and CORS origin for js client

--- src/main/java/com/redparty/partyplanner/controller/constant/WebConstant.java
+++ src/main/java/com/redparty/partyplanner/controller/constant/WebConstant.java
@@ -7,7 +7,9 @@ public interface WebConstant {
     String EVENT_BASE_URL = "event";
     String USER_BASE_URL = "user";
     String SERVICE_BASE_URL = "service";
+    String LOGIN_BASE_URL = "login";
     String BASE_URL = "/";
-    String CORS_URL = "http://postman";
+    String CORS_URL_DEBUG = "http://postman";
+    String CORS_URL = "http://localhost:9005";
 
 }
