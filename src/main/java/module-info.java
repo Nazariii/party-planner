61c027a328cda42b7167c02fa89392156185e494
Nazariii <nazarii.ivashkiv@gmail.com>
2018-01-18 17:35:37 +0200

    Added modularity but still not working (Lombok + some Split Packages issues)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,23 @@
+module com.redparty.partyplanner {
+    requires java.xml.bind;
+    requires lombok;
+    requires java.validation;
+    requires jackson.annotations;
+    requires slf4j.api;
+    requires spring.security.core;
+    requires spring.context.support;
+    requires quartz;
+    requires hibernate.jpa;
+    requires spring.context;
+    requires spring.core;
+    requires spring.security.web;
+    requires tomcat.embed.core;
+    requires com.fasterxml.jackson.databind;
+    requires spring.beans;
+    requires spring.web;
+    requires spring.webmvc;
+    requires spring.boot.autoconfigure;
+    requires spring.security.config;
+    requires java.sql;
+    requires spring.boot;
+}
